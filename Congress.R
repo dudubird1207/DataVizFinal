4615d1aee3c8505bd2fde4c6a064ddc3c5d29ce1
[Elva Huang] <[rongyao.huang@gmial.com]>
2014-04-15 17:34:07 -0400

    clean the source column

--- Congress.R
+++ Congress.R
@@ -59,11 +59,153 @@ final.raw=as.data.frame(final.raw)
 #save the data frame
 save(final.raw,file="final_raw.Rda")
 
+#####---------clean the data frame--------#####
+
+load("final_raw.Rda")
+str(final.raw)
+View(final.raw[6:10,])
+
+#check the number of tags in each source column
+length<-numeric()
+for (i in 1:nrow(final.raw)){
+  length[i]<-length(str_locate_all(final.raw[i,"source"],",")[[1]][,1])
+}
+unique(length)   #0 7 8 9 10 
+
+final.raw[which(length==10)[1],]
+length(which(length==0))
+#Source<-data.frame(nrow=20,ncol=11)
+#for (i in 1:nrow(final.raw)){
+#  index<-c(1,str_locate_all(final.raw[i,"source"],",")[[1]][,1],nchar(final.raw[i,"source"])+1)
+#  if (length(index)==12){
+#    for (j in 1:11){
+#      Source[i,j]<-str_sub(final.raw[i,"source"],index[j]+1,index[j+1]-1)
+#    }
+#  }else if(length(index)==11){
+#    for (j in 1:10){
+#      Source[i,j]<-str_sub(final.raw[i,"source"],index[j]+1,index[j+1]-1)
+#      Source[i,11]<-""
+#    }     
+#  }else if(length(index)==10){
+#    for (j in 1:9){
+#      Source[i,j]<-str_sub(final.raw[i,"source"],index[j]+1,index[j+1]-1)
+#      Source[i,10:11]<-""                                   
+#    }
+#  }else if(length(index)==9){
+#    for (j in 1:8){
+#      Source[i,j]<-str_sub(final.raw[i,"source"],index[j]+1,index[j+1]-1)
+#      Source[i,9:11]<-""
+#    }
+#  }else{
+#    Source[i,1]<-str_sub(final.raw[i,"source"],index[1]+1,index[1+1]-1)
+#    Source[i,2:11]<-""
+#  }
+#}
+
+#test on a short example
+example<-final.raw$source[c(which(length==10)[1],which(length==9)[1],which(length==8)[1],which(length==7)[1],which(length==0)[1])]
+class(example)
+x<-str_split(example,pattern="\",")
+class(x)
+
+#deal with different length
+x<-sapply(x,FUN=function(i){
+  i.v<-unlist(i)
+  if (length(i.v)==6){
+    i.v<-c(i.v,i.v[6])
+    i.v[6]<-NA
+  }
+  return(i.v)
+})
+
+get.source.info <- function(object){
+  data.frame(s1=object[1],s2=object[2],s3=object[3],s5=object[4],s6=object[5],
+             s7=object[6],s8=object[7])
+}
+
+require(plyr)
+source.split <- ldply(x,get.source.info)
+View(source.split)
+
+#split the columns that have two variables
+emptyindex<-which(is.na(source.split$s1=="NA"))
+source.split<-source.split[-emptyindex,]
+source.split$s1<-unlist(str_extract_all(source.split$s1,"name = .*"))
+
+View(source.split)
+new<-ldply(str_split(source.split$s3,pattern=", "),function(x)data.frame(s3=x[1],s4=x[2]))
+source.split<-cbind(source.split[,1:2],new,source.split[,5:7])
+
+names(source.split)<-c("name","locationName","current","government","orgType","stateDelegation","gender")
+
+x<-str_replace_all(as.matrix(source.split),"[[:alpha:]]+ = ","")
+x<-str_replace_all(x,"\"","")
+x[,"gender"]<-str_replace_all(x[,"gender"],")","")
+x<-str_trim(x,side="both")
+View(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vect <- c("Model: Cadilac,Year: 2014", "Model: GMC,Year: 2013,Color: red", "Model: Volt,Year: 2014")
+
+vectList <- str_split(vect, pattern=",")
+vectList
+
+rep(NA, 5-length(x))
+
+
+
+#
+#########------------------------------------------------
+final.raw.save<-final.raw
+final.raw<-final.raw[1:10,]
+Source<-data.frame()
+for (i in 1:nrow(x)){
+  index<-c(5,str_locate_all(x[i,"source"],",")[[1]][,1],nchar(x[i,"source"]))
+ 
+    for (j in 1:11){
+      Source[i,j]<-str_sub(x[i,"source"],index[j]+1,index[j+1]-1)
+    }
+}
+
+index<-c(5,str_locate_all(x[1,"source"],",")[[1]][,1],nchar(x[1,"source"]))
+
+str_sub(x[1,"source"],index[])  
+    
+    
+  for (j in 1:length(index))
+}
+
+for (i in 1:length(index)){
+  <-str_sub(example[1,"source"],6,index[i]-1)
+}
+
+length<-numeric()
+for (i in 1:nrow(final.raw)){
+  length[i]<-length(str_locate_all(final.raw[i,"source"],",")[[1]][,1])
+}
+unqiue(length)
+
+
+
+
+
 #####-----------the rest is just for checking-------------#####
-
-final.raw[200000,]
-
-
 
 
 ff=list()
